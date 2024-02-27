3d61a03d5f94f4625ee0c7dd05674d33fe124edc
marcel <wagmarcel@web.de>
2024-02-27 13:34:17 +0100

    Add index generation script

---
+++ staging/ontology/v0.1/create_index.sh
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+find . -type d -print -exec bash -c 'tree "$0" \
+    -H "." \
+    -L 1 \
+    --noreport \
+    --dirsfirst \
+    --charset utf-8 \
+    -I "index.html" \
+    --ignore-case \
+    --timefmt "%d-%b-%Y %H:%M" \
+    -s \
+    -D \
+    -P "*.ttl|*.jsonld" \
+    -o "$0/index.html"' {} \;
